32c5f0a280ed9244a4793152f8841ff2612861bd
royleguiza <leguizavictorroy@gmail.com>
2025-06-05 23:23:30 -0300

    Product Card

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -9,7 +9,7 @@ function ProductCard(props: ProductCarcProps){
     const { src, title, description, precio} = props;
 
     return (
-        <article className="border border-gray-200 rounded-lg overflow-hidden shadow-sm bg-white w-64 flex flex-col mx-auto mb-6">
+        <article className="border border-gray-200 rounded-lg overflow-hidden shadow-sm bg-white w-64 flex flex-col mx-auto mb-6 m-2">
             <img className="w-full h-64 object-contain p-4" src={src} alt={title} />
             <div className="p-4 flex flex-col items-start">
                 <h1 className="text-base font-normal text-gray-700 mb-2 line-clamp-2">{title}</h1>
