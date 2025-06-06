79ffa40a2578dea16bc53563ffe812af0c0ee62d
royleguiza <leguizavictorroy@gmail.com>
2025-06-06 00:48:52 -0300

    Corregidos Estilos

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,57 @@
-/*import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div className="flex justify-center space-x-4">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App*/
-
 import ProductCard from "./components/ProductCard";
+import ProductCardContainer from "./components/ProductCardContainer";
 
 function App(){
   return (
     <>
-      <ProductCard
-        title="titulo"
-        description="descripcion"
-        src="https://http2.mlstatic.com/D_NQ_NP_857212-MLA76273611895_052024-O.webp"
-        precio={10000}
-      />
+      <ProductCardContainer
+        title="Lo mas Vendido">
+        <ProductCard
+          title="Teclado Mecánico"
+          description="Teclado Mecánico Wireless Redragon Azure K652 Rgb 75% Sp"
+          src="https://http2.mlstatic.com/D_NQ_NP_855390-MLU74275224636_022024-O.webp"
+          precio={90.234}
+        />
 
-      <ProductCard
-        title="titulo"
-        description="descripcion"
-        src="https://http2.mlstatic.com/D_NQ_NP_857212-MLA76273611895_052024-O.webp"
-        precio={10000}
-      />
+        <ProductCard
+          title="Cable Kz Usb C - Pin C"
+          description="Cable Kz Usb C - Pin C Con Microfono Repuesto Original"
+          src="https://http2.mlstatic.com/D_NQ_NP_622684-MLA82301950220_022025-O.webp"
+          precio={16.609}
+        />
 
-      <ProductCard
-        title="titulo"
-        description="descripcion"
-        src="https://http2.mlstatic.com/D_NQ_NP_857212-MLA76273611895_052024-O.webp"
-        precio={10000}
-      />  
+        <ProductCard
+          title="Auriculares KZ gaming ZSN Pro"
+          description="Auriculares KZ gaming ZSN Pro X sin micrófono color negro 1 par"
+          src="https://http2.mlstatic.com/D_NQ_NP_999454-MLA84116925938_052025-O.webp"
+          precio={42.921}
+        /> 
+
+        <ProductCard
+          title="Apple iPad 11 Gen (2025)"
+          description="Apple iPad 11 Gen (2025) Chip A16 11 Pulgadas 6gb Ram 128 Gb"
+          src="https://http2.mlstatic.com/D_NQ_NP_738450-CBT83018743324_032025-O.webp"
+          precio={523.385}
+        /> 
+
+        <ProductCard
+          title="Funda Infantil Para Tablet"
+          description="Funda Infantil Para Tablet Samsung A8 X200 10.5 + Vidrio"
+          src="https://http2.mlstatic.com/D_NQ_NP_857212-MLA76273611895_052024-O.webp"
+          precio={10.564}
+        /> 
+
+        <ProductCard
+          title="Notebook Lenovo Ideapad 1"
+          description="Notebook Lenovo Ideapad 1 15IAU7 Pantalla Táctil Fhd De 15,6 Pulgadas"
+          src="https://http2.mlstatic.com/D_NQ_NP_736072-MLU76955229828_062024-O.webp"
+          precio={819.999}
+        />  
+      </ProductCardContainer>
+      
     </>
   )
 }
+
 
 export default App;
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -9,10 +9,10 @@ function ProductCard(props: ProductCarcProps){
     const { src, title, description, precio} = props;
 
     return (
-        <article className="border border-gray-200 rounded-lg overflow-hidden shadow-sm bg-white w-64 flex flex-col mx-auto mb-6 m-2">
+        <article className="border border-gray-200 rounded-lg overflow-hidden shadow-md bg-white w-64 flex flex-col mx-auto mb-6 m-2">
             <img className="w-full h-64 object-contain p-4" src={src} alt={title} />
             <div className="p-4 flex flex-col items-start">
-                <h1 className="text-base font-normal text-gray-700 mb-2 line-clamp-2">{title}</h1>
+                <h3 className="font-normal text-gray-700 mb-2 line-clamp-2">{title}</h3>
                 <span className="text-2xl font-semibold text-gray-900 mb-1">
                     ${precio}
                 </span>
